98bf213a425b09d924e7dffb8aac6e823505063a
gabi-nunes <69698664+gabi-nunes@users.noreply.github.com>
2020-10-26 16:03:23 -0300

    Add files via upload

---
+++ atividade 10/js.js
@@ -0,0 +1,13 @@
+function Abrir(){
+    document.getElementById("Texto").innerHTML="Abre a janela";
+    document.getElementById("Imagem").src="fechada.jpg";
+}
+
+function fechar(){
+    document.getElementById("Texto").innerHTML="fechaa janela";
+    document.getElementById("Imagem").src="aberta.png";
+}
+function quebrar(){
+    document.getElementById("Texto").innerHTML="quebrar a janela";
+    document.getElementById("Imagem").src="quabrada.jpg";
+}
