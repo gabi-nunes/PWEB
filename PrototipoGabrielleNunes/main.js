d9e3626cee4b4ad504bda82f9a9bbf7802695791
gabi-nunes <69698664+gabi-nunes@users.noreply.github.com>
2020-11-01 17:04:13 -0300

    Add files via upload

---
+++ PrototipoGabrielleNunes/main.js
@@ -0,0 +1,22 @@
+
+function exibirMensagem()
+{
+    
+    var data = new Date();
+    alert(data.toString());
+ 
+
+}
+
+function troca(){
+
+    document.getElementById("Imagem").src="imagem/eu.jpg";
+}
+
+
+alert("Bem vindo ao Portfólio da Gabrielle Nunes!");
+
+function Ti(){
+
+    alert("Obrigada!");
+}
